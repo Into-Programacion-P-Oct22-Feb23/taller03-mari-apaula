2b30e1e58755364ae84d27b94fd21c82bf57532d
Maria Paula Montaño <mpmontano@utpl.edu.ec>
2022-10-30 21:50:59 -0500

    actualizacion2.0

---
+++ Ejercicio08/src/main/java/com/mycompany/ejercicio08/Ejercicio08.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.ejercicio08;
+
+/**
+ *
+ * @author Sistemas G Didactic
+ */
+public class Ejercicio08 {
+
+    public static void main(String[] args) {
+        // TODO code aplication logic here
+        
+        int var1 = 10;
+        int var2 = 9;
+        int var3 = 20;
+        int var4 = 500;
+        int var5 = 2;
+        int var6 = 3;
+        int var7 = 200;
+        int var8 = 50;
+        int var9 = 51;
+        
+        boolean valor = (Math.pow(var1,var2) + var3 - (var4/var5) + Math.pow(var6, var5)) >= var7 || (var8 >= var8 -var8 + var9);
+        System.out.println(valor);
+    
+    }
+}
---
+++ Ejercicio10/src/main/java/com/mycompany/ejercicio10/Ejercicio10.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.ejercicio10;
+
+/**
+ *
+ * @author Sistemas G Didactic
+ */
+public class Ejercicio10 {
+
+    public static void main(String[] args) {
+        // TODE code i
+        
+        int var1 = 25;
+        int var2 = 10;
+        int var3 = 100;
+        int var4 = 5;
+        int var5 = 2;
+        
+        boolean valor = (Math.sqrt(var1) * var2 >= var3);
+        boolean valor2 = true;
+        boolean valor3 = false;
+        boolean valor4 = (var2/var4 >= var5);
+        
+                
+                System.out.println(valor && valor2 || valor3 || valor4);
+    }
+}
